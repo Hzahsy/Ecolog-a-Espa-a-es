bbb30b542d214e0f98a05f2b41dffeca500d444b
Hzahsy <hamzanuaten@gmail.com>
2025-10-08 16:09:03 +0100

    hz

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["3cf9720e-336e-42df-b2da-e043259048b2-00-2tv7zc9sbg1q.kirk.replit.dev"],
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
